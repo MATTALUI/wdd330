a51980346f4373a53d46413a134d3a6c83251892
Matthew Hummer <biggreyson@gmail.com>
2021-09-16 15:17:26 -0600

    improve dynamic link joining

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,7 +3,7 @@
     { url: './week01/index.html', label: 'Week 1 Notes' },
     { url: './week02/notes.html', label: 'Week 2 Notes' },
   ];
-  const contentLinks = linkRegistry.map(link => `<li><a href="${link.url}">${link.label}</a></li>`);
+  const contentLinks = linkRegistry.map(link => `<li><a href="${link.url}">${link.label}</a></li>`).join('');
 
   document.querySelector('#contentList').innerHTML = contentLinks;
 })();
