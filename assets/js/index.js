eae28d2e1939f4c9f48a9432be3b4336e5eccc41
Matthew Hummer <biggreyson@gmail.com>
2021-09-16 15:10:20 -0600

    add week 2 notes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,7 @@
 (() => {
   const linkRegistry = [
     { url: './week01/index.html', label: 'Week 1 Notes' },
+    { url: './week02/notes.html', label: 'Week 2 Notes' },
   ];
   const contentLinks = linkRegistry.map(link => `<li><a href="${link.url}">${link.label}</a></li>`);
 
