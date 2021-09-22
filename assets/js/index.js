1ef4c931aa35cf2b7f93d9b98401d5e0999d06b7
Matthew Hummer <biggreyson@gmail.com>
2021-09-21 20:49:19 -0600

    add week 2 team work

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,7 @@
   const linkRegistry = [
     { url: './week01/index.html', label: 'Week 1 Notes' },
     { url: './week02/notes.html', label: 'Week 2 Notes' },
+    { url: './week02/team.html', label: 'Week 2 Team Assignment' },
   ];
   const contentLinks = linkRegistry.map(link => `<li><a href="${link.url}">${link.label}</a></li>`).join('');
 
---
+++ assets/js/week02.js
@@ -0,0 +1,17 @@
+(() => {
+  const errorMessage = '<span class="error">Your input is invalid.</span>';
+  const isValidNumber = input => !isNaN(+input) && +input >= 0;
+  const factor = num => num > 0 ? num + factor(num - 1) : 0;
+  const getNum = sel => +document.querySelector(sel).value;
+  const setIfValid = (sel, val) => document.querySelector(sel).innerHTML = isValidNumber(val)
+  ? `<span>${val}</span>`
+  : errorMessage;
+  const echo =
+    e => document.querySelector('#echo-results').innerHTML = `<span>${document.querySelector('#echo').value}</span>`;
+  const sumInSeries = e => setIfValid('#sum-in-series-results', factor(getNum('#series')));
+  const sumTwo = e => setIfValid('#sum-two-results', getNum('#add1') + getNum('#add2'));
+
+  document.querySelector('#submit-series').addEventListener('click', sumInSeries);
+  document.querySelector('#submit-echo').addEventListener('click', echo);
+  document.querySelector('#submit-add').addEventListener('click', sumTwo);
+})();
