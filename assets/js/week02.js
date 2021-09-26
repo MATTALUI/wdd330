8bf8a341c16742f0edfbfe13f1c62e0ec62b5074
Matthew Hummer <biggreyson@gmail.com>
2021-09-25 19:09:06 -0600

    fix some error stuff?

--- assets/js/week02.js
+++ assets/js/week02.js
@@ -4,11 +4,13 @@
   const factor = num => num > 0 ? num + factor(num - 1) : 0;
   const getNum = sel => +document.querySelector(sel).value;
   const setIfValid = (sel, val) => document.querySelector(sel).innerHTML = isValidNumber(val)
-  ? `<span>${val}</span>`
-  : errorMessage;
-  const echo =
-    e => document.querySelector('#echo-results').innerHTML = `<span>${document.querySelector('#echo').value}</span>`;
-  const sumInSeries = e => setIfValid('#sum-in-series-results', factor(getNum('#series')));
+    ? `<span>${val}</span>`
+    : errorMessage;
+  const echo = e => document.querySelector('#echo-results').innerHTML =
+    `<span>${document.querySelector('#echo').value}</span>`;
+  const sumInSeries = e => isValidNumber(getNum('#series'))
+    ? setIfValid('#sum-in-series-results', factor(getNum('#series')))
+    : setIfValid('#sum-in-series-results', undefined);
   const sumTwo = e => setIfValid('#sum-two-results', getNum('#add1') + getNum('#add2'));
 
   document.querySelector('#submit-series').addEventListener('click', sumInSeries);
