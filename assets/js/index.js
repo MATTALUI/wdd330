11951437087b89cec1f82aa4bc749e705c09eec9
Matthew Hummer <biggreyson@gmail.com>
2021-09-25 20:41:06 -0600

    add week 03 notes

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,7 @@
     { url: './week01/index.html', label: 'Week 1 Notes' },
     { url: './week02/notes.html', label: 'Week 2 Notes' },
     { url: './week02/team.html', label: 'Week 2 Team Assignment' },
+    { url: './week03/notes.html', label: 'Week 3 Notes' },
   ];
   const contentLinks = linkRegistry.map(link => `<li><a href="${link.url}">${link.label}</a></li>`).join('');
 
