3de11fee7d390e27d459c8d5b81f54b047e52c8e
Matthew Hummer <biggreyson@gmail.com>
2021-11-07 19:36:52 -0700

    week 9 notes

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,6 +14,8 @@
     { url: './week05/team.html', label: 'Week 7 Team Assignment (Hike Comments)' },
     { url: './week08/notes.html', label: 'Week 8 Notes' },
     { url: './week08/team.html', label: 'Week 8 Team Assignment' },
+    { url: './week09/notes.html', label: 'Week 9 Notes' },
+    { url: './week09/team.html', label: 'Week 9 Team Assignment' },
   ];
   const contentLinks = linkRegistry.map(link => `<li><a href="${link.url}">${link.label}</a></li>`).join('');
 
